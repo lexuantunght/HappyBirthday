b58d91e94001c081248dbdac08f3a2eb4e7b7924
tunglx2 <tunglx2@vng.com.vn>
2025-04-16 10:12:19 +0700

    add waiting

--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -4,18 +4,35 @@ import PassLock from 'ui/pass-lock';
 import Render from 'ui/render2';
 import Effects from 'ui/effects';
 import Final from 'ui/final';
+import Countdown from 'ui/countdown';
 import Sound from 'sound/sound-manager';
 
 const App = () => {
-    const [stage, setStage] = React.useState(1);
+    const [stage, setStage] = React.useState(-1);
+    const [count, setCount] = React.useState(0);
 
     React.useEffect(() => {
         Sound.init();
+        fetch('https://timeapi.io/api/time/current/zone?timeZone=Asia%2FSaigon', { method: 'GET' })
+            .then((res) => res.json())
+            .then((res) => {
+                const timeNow = new Date(res.dateTime).getTime();
+                const targetTime = new Date(
+                    import.meta.env.VITE_PUBLIC_TARGET_TIME || '2025-04-17T00:00:00.0000000'
+                ).getTime();
+                if (timeNow < targetTime) {
+                    setCount(Math.round((targetTime - timeNow) / 1000));
+                    setStage(0);
+                } else {
+                    setStage(1);
+                }
+            });
     }, []);
 
     return (
         <AnimatedSwitching classNames="anim-fade" identifier={stage}>
             <div id="app">
+                {stage === 0 && <Countdown value={count} onSuccess={() => setStage(1)} />}
                 {stage === 1 && <PassLock onSuccess={() => setStage(2)} />}
                 {stage === 2 && <Render onSuccess={() => setStage(3)} />}
                 {stage === 3 && <Effects onSuccess={() => setStage(4)} />}
---
+++ src/ui/countdown.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+function renderTime(value) {
+    const h = Math.trunc(value / 3600);
+    const m = Math.trunc((value - h * 3600) / 60);
+    const s = value - h * 3600 - m * 60;
+    return [h, m, s].map((v) => (v > 9 ? v : '0' + v)).join(':');
+}
+
+export default function Countdown(props) {
+    const { value, onSuccess, ...rest } = props;
+    const displayRef = React.useRef(null);
+
+    React.useEffect(() => {
+        let current = value;
+        let timer = null;
+        displayRef.current.textContent = renderTime(current);
+        timer = setInterval(() => {
+            if (current === 0) {
+                clearInterval(timer);
+                onSuccess();
+            } else {
+                current -= 1;
+            }
+            displayRef.current.textContent = renderTime(current);
+        }, 1000);
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
+    return (
+        <div
+            style={{
+                width: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                rowGap: 16,
+            }}>
+            <span style={{ fontSize: 24 }}>Chưa tới lúc, vui lòng đợi :D</span>
+            <span
+                style={{ fontSize: 64, fontWeight: 600, fontFamily: 'Arial' }}
+                {...rest}
+                ref={displayRef}></span>
+        </div>
+    );
+}
